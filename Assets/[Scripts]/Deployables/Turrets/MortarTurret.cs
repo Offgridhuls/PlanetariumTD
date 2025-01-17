eddd0e768284428100c945c3f503321547ba5cfc
blafork <juliansangiorgio@Outlook.com>
2025-01-17 14:19:52 -0500

    Fixed mortar turret prefab

---
+++ Assets/[Scripts]/Deployables/Turrets/MortarTurret.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MortarTurret : DeployableBase
+{
+    protected override void FireTurret()
+    {
+        base.FireTurret();
+    }
+}
